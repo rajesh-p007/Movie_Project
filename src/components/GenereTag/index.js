98532fdfbda62d056dc0a8f53a5f9e4ad41995f2
= <=>
2020-04-08 21:00:07 +0530

    Second Commit

--- src/components/GenereTag/index.js
+++ src/components/GenereTag/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
 import './GenereTag.scss';
 
 const Tag = ({ text }) => (<div className="tag">{text}</div>);
-
-Tag.propTypes = {
-  text: PropTypes.string.isRequired,
-}
 
 export default Tag;
